094611f7c8e6ea4ce437c4e16d485b448dec11ac
Wilson Work <xuwcun@163.com>
2016-02-29 11:28:17 +0800

    Added some operation handle functions

--- bc/Admin/Controller/BusinessController.class.php
+++ bc/Admin/Controller/BusinessController.class.php
@@ -17,22 +17,25 @@ class BusinessController extends Controller {
 		//generate the page 
 		$this->display();
 	}
-	public function getDeviceSize()
-	{
-		$size['level'] = 1;
-		$this->AjaxReturn($size);
-	} 
-	public function get_disk_info()
+	
+	public function getDiskInfo()
 	{
 		 //initiate database   --generate model
 		$db = M('Device');
 		$rooms = $db->select();
-		
+		$returnData = array();
 		foreach($rooms as $item)
 		{
-			$data['id'] = $item['id'];
-			$db->save($data);
+			//add all the in-position disks into the return data array
+			if($item['loaded'] == 1)
+			{
+				//硬盘在位
+				$returnData[] = $item;
+			} 
+			
 		}
+		$this->AjaxReturn($returnData);
+		
 	}
 	/***
 	* to check if or not the same commond has been sent
@@ -63,11 +66,120 @@ class BusinessController extends Controller {
 	* to insert a new commond-request log, driven by javascript
 	* @input: cmd, subcmd, 
 	*/
-	public function updateCmdLog()
+	public function AddCmdLog()
 	{
+		$content_type_args = explode(';', $_SERVER['CONTENT_TYPE']);
+	   if ($content_type_args[0] == 'application/json')
+	   {
+		   $_POST = json_decode(file_get_contents('php://input'),true);     
+	   }
+	   $db = M('CmdLog');
+		$data['cmd'] = I('post.cmd');
+		$data['subcmd'] = I('post.subcmd');
+		$data['status'] = -1;//-1 represents that the commond is not finished yet.
+		$id = $db->add($data);
+		if($id)
+		{
+			//insert related disk rooms
+			
+		}
+		else
+		{
+			//throw an exception
+		}
 		
 	}
-	public function pick_dist(){   
+	/***
+	* add the disks refered in an commond excutation
+	* @author: wilson xu
+	* @input: posted levels or level group disk or level group disks
+	* @input: sent commond id
+	*/
+	public function cmdReferedDisks($id)
+	{
+		$levels = I('post.levels');
+		$db = M('CmdDisks');
+		$data['id'] = $id;
+		if($levels)
+		{			
+			foreach($levels as $item)
+			{
+				$data['level'] = $item;
+				$this->handleGroup($data,$db);
+			}
+		}
+		else
+		{
+			$level = I('post.level');
+			if($level)
+			{
+				$data['level'] = $level;
+				$this->handleGroup($data,$db);
+			}
+			else
+			{
+				//something wrong
+				$data['error'] = "No disk assigned to the commond. Recheck the code.";
+				$this->AjaxReturn($data);
+				
+			}
+		}
+		
+	}
+	private function handleGroup($data,$db)
+	{
+		$groups = I('post.groups');
+		if($groups)
+		{
+			foreach($groups as $group)
+			{
+				$data['group'] = $group;
+				$this->handleDisk($data,$db);
+
+			}
+
+
+		}
+		else
+		{
+
+			$group = I('post.group');
+			if($group)
+			{
+				$data['group'] = $group;
+				$this->handleDisk($data,$db);
+			}
+			else
+			{
+				$db->add($data);
+			}
+
+		}
+				
+	}
+	private function handleDisk($data,$db)
+	{
+		$disks = I('post.disks');
+		if($disks)
+		{
+			foreach($disks as $disk)
+			{
+				$data['disk'] = $disk; 
+				$db->add($data);
+			}
+		}
+		else
+		{
+			$disk = I('post.disk');
+			if($disk)
+			{
+				$data['disk'] = $disk;
+			}
+			$db->add($data);
+		}
+	}
+	
+	public function pick_disk(){   
 		//check permission
 		//query database
 		//return 
