175a3fc56226cf77aa61b8e132c8249ed9a83f07
xuweicun <xuwcun@163.com>
2016-04-17 19:29:40 +0800

    a lot of changes

--- bc/Admin/Conf/db.inc.php
+++ bc/Admin/Conf/db.inc.php
@@ -4,7 +4,7 @@ return array (
   'DB_HOST' => 'localhost',
   'DB_PORT' => '3306',
   'DB_NAME' => 'gui',
-  'DB_USER' => 'lordinwar',
+  'DB_USER' => 'root',
   'DB_PWD' => 'moganfreeman',
   'DB_PREFIX' => 'gui_',
   'DB_CHARSET'=>'utf8'
