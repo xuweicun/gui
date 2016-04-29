a747b33347aaca882cb03038cf449c8e0f75c1a6
xuweicun <xuwcun@163.com>
2016-04-29 23:02:25 +0800

    修复前端任务池

--- Public/js/globaljs.js
+++ Public/js/globaljs.js
@@ -1,5 +1,31 @@
-angular.module('device.controllers', [])
-    .controller('statusMonitor', function ($scope, $http, $interval, Lang, TestMsg) {
+angular.module('device.controllers', ['datatables'])
+    .controller('statusMonitor', function ($scope, $http, $interval, Lang, TestMsg, DTOptionsBuilder, DTDefaultOptions) {
+        var vm = this;
+        vm.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers');
+        DTDefaultOptions.setLanguage({
+            "sProcessing": "处理中...",
+            "sLengthMenu": "每页显示 _MENU_ 条命令",
+            "sZeroRecords": "没有匹配的命令",
+            "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+            "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+            "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+            "sInfoPostFix": "",
+            "sUrl": "",
+            "sEmptyTable": "当前没有执行中的命令",
+            "sLoadingRecords": "载入中...",
+            "sInfoThousands": ",",
+            "oPaginate": {
+                "sFirst": "首页",
+                "sPrevious": "上页",
+                "sNext": "下页",
+                "sLast": "末页"
+            },
+            "oAria": {
+                "sSortAscending": ": 以升序排列此列",
+                "sSortDescending": ": 以降序排列此列"
+            }
+        });
+
         //服务器错误信息池，格式[{errMsg:'err'},{errMsg:'err'}]
         $scope.user = $("#userid").val();
         $scope.testMsg = TestMsg;
