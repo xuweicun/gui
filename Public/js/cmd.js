ec35bc51381c487869aedc9d92508586f41e67f3
xuweicun <xuwcun@163.com>
2016-05-01 16:32:47 +0800

    前端代码重构

--- Public/js/cmd.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Wilson Xu on 2016/2/23.
- * All the commonds should be listed here.
- */
-function disk_info(level,group,index) {
-
-
-}
---
+++ Public/js/globalvars.js
@@ -0,0 +1,4 @@
+﻿var app = angular.module('device', ['device.controllers', 'device.services']);
+var app_device = angular.module('device.controllers', ['datatables']);
+var global_cmd_helper;
+var global_cabinet;
