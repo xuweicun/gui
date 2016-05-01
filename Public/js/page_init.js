9ca4a2d6c4b2c08c5f35329a57787738b476d8f2
xuweicun <xuwcun@163.com>
2016-05-01 15:37:16 +0800

    将模块拆分到不同js文件中

--- Public/js/page_init.js
+++ Public/js/page_init.js
@@ -1,115 +1,84 @@
-﻿
-// Data Tables - Config
-(function ($) {
-
-    'use strict';
-
-    // we overwrite initialize of all datatables here
-    // because we want to use select2, give search input a bootstrap look
-    // keep in mind if you overwrite this fnInitComplete somewhere,
-    // you should run the code inside this function to keep functionality.
-    //
-    // there's no better way to do this at this time :(
-    if ($.isFunction($.fn['dataTable'])) {
-
-        $.extend(true, $.fn.dataTable.defaults, {
-            sDom: "<'row datatables-header form-inline'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>r><'table-responsive't><'row datatables-footer'<'col-sm-12 col-md-6'i><'col-sm-12 col-md-6'p>>",
-            oLanguage: {
-                sLengthMenu: '_MENU_ records per page',
-                sProcessing: '<i class="fa fa-spinner fa-spin"></i> Loading'
-            },
-            fnInitComplete: function (settings, json) {
-                // select 2
-                if ($.isFunction($.fn['select2'])) {
-                    $('.dataTables_length select', settings.nTableWrapper).select2({
-                        minimumResultsForSearch: -1
-                    });
-                }
-
-                var options = $('table', settings.nTableWrapper).data('plugin-options') || {};
-
-                // search
-                var $search = $('.dataTables_filter input', settings.nTableWrapper);
-
-                $search
-					.attr({
-					    placeholder: typeof options.searchPlaceholder !== 'undefined' ? options.searchPlaceholder : '搜索'
-					})
-					.addClass('form-control');
-
-                if ($.isFunction($.fn.placeholder)) {
-                    $search.placeholder();
-                }
-            }
-        });
-
-    }
-
-}).apply(this, [jQuery]);
-
-
-/*Datatable - default*/
-(function ($) {
-
-    'use strict';
-
-    var datatableInit = function () {
-
-        $('#datatable-default').dataTable({
-            language: {
-                "sProcessing": "处理中...",
-                "sLengthMenu": "每页显示 _MENU_ 条命令",
-                "sZeroRecords": "没有匹配的命令",
-                "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
-                "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
-                "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
-                "sInfoPostFix": "",
-                "sUrl": "",
-                "sEmptyTable": "当前没有执行中的命令",
-                "sLoadingRecords": "载入中...",
-                "sInfoThousands": ",",
-                "oPaginate": {
-                    "sFirst": "首页",
-                    "sPrevious": "上页",
-                    "sNext": "下页",
-                    "sLast": "末页"
-                },
-                "oAria": {
-                    "sSortAscending": ": 以升序排列此列",
-                    "sSortDescending": ": 以降序排列此列"
-                }
-            }
-        });
-
-        $('#table-done-tasks').dataTable({
-            language: {
-                "sProcessing": "处理中...",
-                "sLengthMenu": "每页显示 _MENU_ 条命令",
-                "sZeroRecords": "没有匹配的命令",
-                "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
-                "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
-                "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
-                "sInfoPostFix": "",
-                "sUrl": "",
-                "sEmptyTable": "当前没有已完成的命令",
-                "sLoadingRecords": "载入中...",
-                "sInfoThousands": ",",
-                "oPaginate": {
-                    "sFirst": "首页",
-                    "sPrevious": "上页",
-                    "sNext": "下页",
-                    "sLast": "末页"
-                },
-                "oAria": {
-                    "sSortAscending": ": 以升序排列此列",
-                    "sSortDescending": ": 以降序排列此列"
-                }
-            }
-        });
-    };
-
-    $(function () {
-        datatableInit();
+﻿app_device.controller('InitCtrl', function ($scope, DTOptionsBuilder, DTDefaultOptions) {
+    var vm = this;
+    vm.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers');
+    DTDefaultOptions.setLanguage({
+        "sProcessing": "处理中...",
+        "sLengthMenu": "每页显示 _MENU_ 条命令",
+        "sZeroRecords": "没有匹配的命令",
+        "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+        "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+        "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+        "sInfoPostFix": "",
+        "sUrl": "",
+        "sEmptyTable": "当前没有执行中的命令",
+        "sLoadingRecords": "载入中...",
+        "sInfoThousands": ",",
+        "oPaginate": {
+            "sFirst": "首页",
+            "sPrevious": "上页",
+            "sNext": "下页",
+            "sLast": "末页"
+        },
+        "oAria": {
+            "sSortAscending": ": 以升序排列此列",
+            "sSortDescending": ": 以降序排列此列"
+        }
     });
 
-}).apply(this, [jQuery]);
+    function init_popup() {
+        $('.modal-with-move-anim').magnificPopup({
+            type: 'inline',
+
+            fixedContentPos: false,
+            fixedBgPos: true,
+
+            overflowY: 'auto',
+
+            closeBtnInside: true,
+            preloader: false,
+
+            midClick: true,
+            removalDelay: 300,
+            mainClass: 'my-mfp-slide-bottom',
+            modal: true
+        });
+
+        $(document).on('click', '.modal-dismiss', function (e) {
+            e.preventDefault();
+            $.magnificPopup.close();
+        });
+
+        $(".chk-radios-form").each(function () {
+            $(this).validate({
+                highlight: function (element) {
+                    $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+                },
+                success: function (element) {
+                    $(element).closest('.form-group').removeClass('has-error');
+                }
+            })
+        });
+
+        $.extend($.validator.messages, {
+            required: "（必填）",
+            remote: "请修正此字段",
+            email: "请输入有效的电子邮件地址",
+            url: "请输入有效的网址",
+            date: "请输入有效的日期",
+            dateISO: "请输入有效的日期 (YYYY-MM-DD)",
+            number: "请输入有效的数字",
+            digits: "只能输入数字",
+            creditcard: "请输入有效的信用卡号码",
+            equalTo: "你的输入不相同",
+            extension: "请输入有效的后缀",
+            maxlength: $.validator.format("最多可以输入 {0} 个字符"),
+            minlength: $.validator.format("最少要输入 {0} 个字符"),
+            rangelength: $.validator.format("请输入长度在 {0} 到 {1} 之间的字符串"),
+            range: $.validator.format("请输入范围在 {0} 到 {1} 之间的数值"),
+            max: $.validator.format("请输入不大于 {0} 的数值"),
+            min: $.validator.format("请输入不小于 {0} 的数值")
+        });
+    }
+
+    init_popup();
+});
