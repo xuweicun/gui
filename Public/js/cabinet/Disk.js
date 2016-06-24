70709cc1fa5e8752102f640f59cf4cd2482296f2
clothepole <hxlinkai@126.com>
2016-06-24 19:37:43 +0800

    test

--- Public/js/cabinet/Disk.js
+++ Public/js/cabinet/Disk.js
@@ -556,21 +556,21 @@ Disk.prototype = {
 */
         if (this.curr_cmd) {
         	var time_text = '';
-		var use_time = this.curr_cmd.usedTime;
-		if (use_time)
-		if (use_time < 60){
-			time_text = use_time + 's';
-		}
-		else if (use_time < 3600){
-			time_text = (use_time / 60).toFixed(2) + 'm';
-		}
-		else if (use_time < 3600 * 24){
-			time_text = (use_time / 3600).toFixed(2) + 'h';
-		}
-		else {
-			time_text = (use_time / 3600 / 24).toFixed(2) + 'd';
-		}    
-		ex_title = 'T: ' + time_text + '，P: ' + this.curr_cmd.progress + '%';
+			var use_time = this.curr_cmd.usedTime;
+			if (use_time)
+			if (use_time < 60){
+				time_text = use_time + 's';
+			}
+			else if (use_time < 3600){
+				time_text = (use_time / 60).toFixed(2) + 'm';
+			}
+			else if (use_time < 3600 * 24){
+				time_text = (use_time / 3600).toFixed(2) + 'h';
+			}
+			else {
+				time_text = (use_time / 3600 / 24).toFixed(2) + 'd';
+			}    
+			ex_title = 'T: ' + time_text + '，P: ' + this.curr_cmd.progress + '%';
         }
 
         var _name = this.get_cmd_name();
