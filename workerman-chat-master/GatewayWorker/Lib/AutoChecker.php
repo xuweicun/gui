0df2fd98506a44bb6b906285756f5dacb8cba8b0
WilsonXu <xuwcun@163.com>
2016-07-15 16:50:07 +0800

    md5 check chged

--- workerman-chat-master/GatewayWorker/Lib/AutoChecker.php
+++ workerman-chat-master/GatewayWorker/Lib/AutoChecker.php
@@ -112,6 +112,7 @@ Class AutoChecker
             $this->RunLog("Check start time.");
             //检查自检时间是否已经更新,如果未更新则更新
             $curr_start_t = $db->select("start_date")->from($this->tbl_start_date)->where("type=:T and is_current=1")->bindValues(array('T' => $this->type))->single();
+            $this->RunLog("Check finished");
             if($curr_start_t)
             {
                 $this->RunLog("Current start time:".$curr_start_t['start_date']);
